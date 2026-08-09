daad294583f9a039da1235b11cec4990cae87a98
Mateusz Kadlubowski <xeho91@pm.me>
2026-08-09 10:20:48 +0200

    Advertise `DisabledSupport` in default test client capabilities

--- internal/fourslash/fourslash.go
+++ internal/fourslash/fourslash.go
@@ -479,6 +479,9 @@ func GetDefaultCapabilities() *lsproto.ClientCapabilities {
 			HoverVerbosityLevel: ptrTrue,
 		},
 		TextDocument: &lsproto.TextDocumentClientCapabilities{
+			CodeAction: &lsproto.CodeActionClientCapabilities{
+				DisabledSupport: ptrTrue,
+			},
 			Completion: &lsproto.CompletionClientCapabilities{
 				CompletionItem: &lsproto.ClientCompletionItemOptions{
 					SnippetSupport:          ptrFalse,
